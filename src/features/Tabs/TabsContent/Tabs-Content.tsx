230c770cec1067b0c41e5bbfcb038ff3851dbe5d
Julia.Maksimova <Julia.Maksimova@russianpost.ru>
2023-10-28 20:16:14 +0300

    feat: добавлен комментарий

--- src/features/Tabs/TabsContent/Tabs-Content.tsx
+++ src/features/Tabs/TabsContent/Tabs-Content.tsx
@@ -6,6 +6,7 @@ const offersList = offers.slice(0, 5);
 export const TabsContent = () => (
   <div className="cities">
     <div className="cities__places-container container">
+      {/* здесь IDE ругается на тип data непонятно почему, хотя он прописан */}
       <OffersList data={offersList}/>
       <div className="cities__right-section">
         <section className="cities__map map"></section>
