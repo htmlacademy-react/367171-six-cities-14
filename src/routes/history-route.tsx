69ce9ab87b63346bc09efc7fff4532da876b74c5
Julia.Maksimova <Julia.Maksimova@russianpost.ru>
2023-11-03 14:19:08 +0300

    feat: добавлен компонент history-route
    (cherry picked from commit 841dd28777af71807877e92e37321ab579c10abc)

--- src/routes/history-route.tsx
+++ src/routes/history-route.tsx
@@ -33,3 +33,16 @@ function HistoryRouter({
 }
 
 export default HistoryRouter;
+
+import { MemoryHistory, createMemoryHistory } from 'history';
+import HistoryRouter from '../components/history-route/history-route';
+
+export function withHistory(component: JSX.Element, history?: MemoryHistory) {
+  const memoryHistory = history ?? createMemoryHistory();
+
+  return (
+    <HistoryRouter history={memoryHistory}>
+      {component}
+    </HistoryRouter>
+  );
+}
