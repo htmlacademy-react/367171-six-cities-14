ed33e003e0ff122b3032e828303a6868648c8e2f
cybersunt <juliam2007@mail.ru>
2023-11-06 18:25:27 +0300

    feat: добавлено тестирование компонента location-item

---
+++ src/entities/location/ui/LocationItem/location-item.test..tsx
@@ -0,0 +1,27 @@
+import './location-item.css';
+import { render, screen } from '@testing-library/react';
+import {LocationItem} from './location-item';
+
+describe('Component: LocationItem', () => {
+  it('should render correctly', () => {
+    const mockActiveKey = 2;
+    const mockId = 1;
+    const mockTitle = 'Paris';
+    const mockHandleClick = vi.fn();
+
+    render(<LocationItem id={mockId} title={mockTitle} activeKey={mockActiveKey} onClick={mockHandleClick}/>);
+
+    expect(screen.getByText(mockTitle)).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toHaveClass('tabs__item--active');
+    expect(screen.getByRole('li')).toHaveClass('locations__item');
+  });
+  it('should render correctly not active class', () => {
+    const mockActiveKey = 2;
+    const mockId = 1;
+    const mockTitle = 'Paris';
+
+    render(<LocationItem id={mockId} title={mockTitle} activeKey={mockActiveKey}/>);
+    expect(screen.getByRole('button')).not.toHaveClass('tabs__item--active');
+  });
+});
